572bc65838c124e37dfc131b46594caf7808b38d
Khalil Samba <khalil.samba@centrale-casablanca.ma>
2025-01-05 16:05:34 +0100

    mise a jour

--- core/model.py
+++ core/model.py
@@ -4,6 +4,9 @@ import numpy as np
 import datetime as dt
 from numpy import newaxis
 from core.utils import Timer
+
+import tensorflow as tf
+from tensorflow import keras
 from keras.layers import Dense, Activation, Dropout, LSTM
 from keras.models import Sequential, load_model
 from keras.callbacks import EarlyStopping, ModelCheckpoint
